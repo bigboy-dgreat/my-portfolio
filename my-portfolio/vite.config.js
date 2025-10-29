ed758cac5cf2c7b3969e4517cc548a696865458d
https://github.com/bigboy-dgreat/ThaGreat <jinadbstefan@gmail.com>
2025-10-29 16:14:16 +0100

    Updated title and metadata

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/', // 👈 must match your repo name
 })
+
+
